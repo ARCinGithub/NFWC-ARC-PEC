0e2c9d198deac58ff9b4580065508cb823c56f8e
ARCinGithub <fabricsun@126.com>
2025-12-02 21:06:24 +0800

    Add recipe for organ unbrilliant glory

---
+++ kubejs/server_scripts/arc_expansion/arc_recipes/arc_recipes.js
@@ -0,0 +1,12 @@
+ServerEvents.recipes((event) => {
+	// "未照耀的荣光"
+	event.recipes.summoningrituals
+		.altar("kubejs:ritual_catalyst")
+		.id("arc_expansion:unbrilliant_glory")
+		.input("4x minecraft:gold_block")
+		.input("4x cataclysm:ignitium_ingot")
+		.input("minecraft:golden_sword")
+		.input("4x createaddition:gold_rod")
+		.itemOutput("arc_expansion:unbrilliant_glory")
+		.recipeTime(500);
+});
